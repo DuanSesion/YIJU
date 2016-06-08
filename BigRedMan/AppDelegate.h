3acecade98c18138275803276b4a1c1445ed1796
duanshaoxiong <duanshaoxiong@tangdada.cn>
2016-06-08 14:39:48 +0800

    Initial Commit

---
+++ BigRedMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigRedMan
+//
+//  Created by Duanshaoxiong on 16/6/8.
+//  Copyright © 2016年 Duanshaoxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BigRedMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BigRedMan
+//
+//  Created by Duanshaoxiong on 16/6/8.
+//  Copyright © 2016年 Duanshaoxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
